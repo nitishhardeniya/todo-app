5870822ba15b1a038ee87ce811cdfe9e6d3aa96d
Nitish <Nitish>
2018-06-11 16:00:11 +0530

    todo creation

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,12 +1,34 @@
-todo.controller('homeController',function($scope,todoService){
-	$scope.name = "Nitish";
+todo.controller('homeController',function($scope,$uibModal,todoService){
+	
+	$scope.taskData = {
+		dueDate:new Date()
+	};
+
+	var modalInstance;
+	var modalScope = $scope.$new();
 	$scope.addTodo = function(){
-		$scope.todos.push({"id":$scope.todos.length+1,"title":"Release to production","status":"done","desc":"Short dec","assignedTo":"Nitish"});
+		/*modalInstance = $uibModal.open({
+			templateUrl: 'views/create-task.html',
+			ariaLabelledBy: 'modal-title',
+      		ariaDescribedBy: 'modal-body',
+			size:'md',
+			scope:modalScope
+		});
+
+		modalScope.createTask = function(){
+		};*/
+
+		$scope.todos.push({"id":$scope.todos.length+1,"title":$scope.taskData.title,"status":"pending","desc":$scope.taskData.desc,"dueDate":$scope.taskData.dueDate,"assignedTo":"Nitish"});
+		$scope.taskData = {};
+	};
+
+	$scope.closeModal = function(){
+		modalInstance.dismiss('cancel');
 	};
 
 	$scope.todos = [{"id":"1","title":"Create page UI","status":"pending","desc":"Short dec","assignedTo":"Nitish"},
-					{"id":"2","title":"Style the page","status":"pending","desc":"Short dec","assignedTo":"Nitish"},
-					{"id":"3","title":"Test the functionality","status":"pending","desc":"Short dec","assignedTo":"Nitish"}]
+					{"id":"2","title":"Style the page","status":"in-progress","desc":"Short dec","assignedTo":"Nitish"},
+					{"id":"3","title":"Test the functionality","status":"pending","desc":"Short dec","assignedTo":"Nitish"}];
 
 	$scope.removeTask = function(taskIndex){
 		$scope.todos.splice(taskIndex,1);
