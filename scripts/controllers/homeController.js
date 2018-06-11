e4855499191ac9a12454552107ff97dd6adcd095
Nitish <Nitish>
2018-06-11 22:01:52 +0530

    assignee

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,4 +1,4 @@
-todo.controller('homeController',function($scope,$uibModal,todoService){
+todo.controller('homeController',function($scope,todoService){
 	
 	$scope.taskData = {
 		dueDate:new Date()
@@ -7,34 +7,32 @@ todo.controller('homeController',function($scope,$uibModal,todoService){
 	var modalInstance;
 	var modalScope = $scope.$new();
 	$scope.addTodo = function(){
-		/*modalInstance = $uibModal.open({
-			templateUrl: 'views/create-task.html',
-			ariaLabelledBy: 'modal-title',
-      		ariaDescribedBy: 'modal-body',
-			size:'md',
-			scope:modalScope
-		});
-
-		modalScope.createTask = function(){
-		};*/
-
-		$scope.todos.push({"id":$scope.todos.length+1,"title":$scope.taskData.title,"status":"pending","desc":$scope.taskData.desc,"dueDate":$scope.taskData.dueDate,"assignedTo":"Nitish"});
+		
+		$scope.todos.push({"id":$scope.todos.length+1,"title":$scope.taskData.title,"status":"pending","desc":$scope.taskData.desc,"dueDate":$scope.taskData.dueDate,"assignedTo":$scope.taskData.assignedTo});
 		$scope.taskData = {
 			dueDate:new Date()
 		};
 	};
 
-	$scope.closeModal = function(){
-		modalInstance.dismiss('cancel');
-	};
+	$scope.users = todoService.getUserList();
 
 	//Pre-loaded dummy data
-	$scope.todos = [{"id":"1","title":"Create page UI","status":"pending","desc":"Short dec sdfdsfdsj dsjfkdsj kfj ds","dueDate":new Date(),"assignedTo":"Nitish"},
-					{"id":"2","title":"Style the page","status":"in-progress","desc":"Short dec sjksdjfkjds ","dueDate":new Date(),"assignedTo":"Nitish"},
-					{"id":"3","title":"Test the functionality","status":"pending","desc":"Short dec pplplpasldplasd ","dueDate":new Date(),"assignedTo":"Nitish"}];
-
+	$scope.todos = [];
+	if($scope.todos.length==0){
+		console.log('setter');
+		$scope.todos = todoService.getPreloadedData();
+		localStorage.setItem("todosMaster", $scope.todos);
+	}else{
+		console.log('getter');
+		$scope.todos = localStorage.getItem("todosMaster");
+	}
+	
 	$scope.removeTask = function(taskIndex){
 		$scope.todos.splice(taskIndex,1);
+	};
+
+	$scope.updateStatus = function(taskIndex){
+		$scope.todos[taskIndex].status = "done";
 	};
 
 	$scope.getTaskDetails = function(taskIndex){
--- scripts/services/todoService.js
+++ scripts/services/todoService.js
@@ -1,3 +1,13 @@
 todo.service('todoService', ['$http', function($http){
-	
-}])
+	return {
+		getPreloadedData : function(){
+			return [{"id":"1","title":"Create page UI","status":"pending","desc":"Short dec sdfdsfdsj dsjfkdsj kfj ds","dueDate":new Date(),"assignedTo":"Nitish"},
+					{"id":"2","title":"Style the page","status":"in-progress","desc":"Short dec sjksdjfkjds ","dueDate":new Date(),"assignedTo":"Nitish"},
+					{"id":"3","title":"Test the functionality","status":"pending","desc":"Short dec pplplpasldplasd ","dueDate":new Date(),"assignedTo":"Nitish"}];
+		},
+
+		getUserList : function(){
+			return [{"userId":1,"name":"Nitish Hardeniya"},{"userId":1,"name":"Akash neekhra"},{"userId":1,"name":"Gaurav gupta"}];
+		}
+	}
+}]);
